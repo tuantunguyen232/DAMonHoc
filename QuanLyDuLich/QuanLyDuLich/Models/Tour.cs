6432da82ae2c4b2520c17c5bbcdd9fbbc927d318
tuantunguyen232 <tuantu.nguyenq23@gmail.com>
2023-09-29 13:57:43 +0700

    first creation

---
+++ QuanLyDuLich/QuanLyDuLich/Models/Tour.cs
@@ -0,0 +1,59 @@
+namespace QuanLyDuLich.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Tour")]
+    public partial class Tour
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Tour()
+        {
+            Comment = new HashSet<Comment>();
+            Order_details = new HashSet<Order_details>();
+            Tour_image = new HashSet<Tour_image>();
+        }
+
+        [Key]
+        [StringLength(10)]
+        public string id_tour { get; set; }
+
+        [StringLength(100)]
+        public string tour_name { get; set; }
+
+        [StringLength(255)]
+        public string tour_description { get; set; }
+
+        public decimal? price { get; set; }
+
+        [StringLength(20)]
+        public string status { get; set; }
+
+        public DateTime? departure_time { get; set; }
+
+        public DateTime? return_time { get; set; }
+
+        [StringLength(255)]
+        public string image { get; set; }
+
+        [StringLength(100)]
+        public string address { get; set; }
+
+        [Column(TypeName = "numeric")]
+        public decimal? rating { get; set; }
+
+        public int? is_active { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Comment> Comment { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order_details> Order_details { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Tour_image> Tour_image { get; set; }
+    }
+}
---
+++ QuanLyDuLich/QuanLyDuLich/Models/User.cs
@@ -0,0 +1,60 @@
+namespace QuanLyDuLich.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            Comment = new HashSet<Comment>();
+            Like_action = new HashSet<Like_action>();
+            News_comment = new HashSet<News_comment>();
+            Order = new HashSet<Order>();
+        }
+
+        [Key]
+        [StringLength(10)]
+        public string id_user { get; set; }
+
+        [StringLength(20)]
+        public string first_name { get; set; }
+
+        [StringLength(20)]
+        public string last_name { get; set; }
+
+        [StringLength(20)]
+        public string username { get; set; }
+
+        [StringLength(20)]
+        public string password { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? date_of_birth { get; set; }
+
+        [StringLength(20)]
+        public string user_role { get; set; }
+
+        [StringLength(255)]
+        public string image { get; set; }
+
+        public int? is_active { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Comment> Comment { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Like_action> Like_action { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<News_comment> News_comment { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Order { get; set; }
+    }
+}
