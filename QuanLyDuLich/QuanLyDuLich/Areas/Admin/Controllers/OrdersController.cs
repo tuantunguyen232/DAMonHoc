db535c359c10d8ec2fc66842f38c1ef62839a6aa
tuantunguyen232 <tuantu.nguyenq23@gmail.com>
2023-10-20 16:26:10 +0700

    commit admin

---
+++ QuanLyDuLich/QuanLyDuLich/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyDuLich.Models;
+
+namespace QuanLyDuLich.Areas.Admin.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private DuLichDB db = new DuLichDB();
+
+        // GET: Admin/Orders
+        public ActionResult Index()
+        {
+            var order = db.Order.Include(o => o.User);
+            return View(order.ToList());
+        }
+
+
+        // GET: Admin/Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Order.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_user = new SelectList(db.User, "id_user", "first_name");
+            return View();
+        }
+
+        // POST: Admin/Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_orders,created_date,total,id_user,state")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Order.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_user = new SelectList(db.User, "id_user", "first_name", order.id_user);
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Order.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_user = new SelectList(db.User, "id_user", "first_name", order.id_user);
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_orders,created_date,total,id_user,state")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_user = new SelectList(db.User, "id_user", "first_name", order.id_user);
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Order.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Order.Find(id);
+            db.Order.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ QuanLyDuLich/QuanLyDuLich/Areas/Admin/Controllers/ToursController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using QuanLyDuLich.Areas.Admin.Service;
+using QuanLyDuLich.Models;
+
+namespace QuanLyDuLich.Areas.Admin.Controllers
+{
+    public class ToursController : Controller
+    {
+        private DuLichDB db = new DuLichDB();
+        private TourService tourService = new TourService();
+
+        // GET: Admin/Tours
+        public ActionResult TourList()
+        {
+            List<Tour> tours = tourService.getAll();
+            return View(tours);
+        }
+
+        public ActionResult TourReview()
+        {
+            using (var db = new DuLichDB())
+            {
+                var inactiveTours = db.Tour.Where(t => t.is_active == false).ToList();
+                return View(inactiveTours);
+            }
+        }
+
+        // GET: Admin/Tours/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tour.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        // GET: Admin/Tours/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Tours/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Tour tour)
+        {
+            try 
+            {
+                bool check = tourService.save(tour);
+
+                if (check)
+                {
+                    TempData["SuccessMessage"] = "Lưu thành công!";
+                    return RedirectToAction("TourList");
+                }
+                else
+                {
+                    TempData["FailMessage"] = "Lưu không thành công!";
+                    return RedirectToAction("Create");
+                }
+            }
+            catch 
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Tours/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tour.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        //private ActionResult View(Tour tour)
+        //{
+        //    ViewBag.tour = tour;
+        //    return View();
+        //}
+
+        // POST: Admin/Tours/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit([Bind(Include = "id_tour,tour_name,tour_description,tour_schedule,price,status,departure_time,return_time,departure_location,arrival_location,image,quantity,is_active")] Tour tour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tour).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("TourList");
+            }
+            return View(tour);
+        }
+
+        // GET: Admin/Tours/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tour.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        // POST: Admin/Tours/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tour tour = db.Tour.Find(id);
+            db.Tour.Remove(tour);
+            db.SaveChanges();
+            return RedirectToAction("TourList");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult ActiveTour(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tour.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        [HttpPost, ActionName("ActiveTour")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ActiveConfirm(int id)
+        {
+            try
+            {
+                TempData["ActiveSuccess"] = "Đã duyệt thành công!";
+                Tour tour = db.Tour.Find(id);
+                tour.is_active = true;
+                db.SaveChanges();
+                return RedirectToAction("TourReview");
+            } 
+            catch   
+            {
+                 TempData["ActiveFail"] = "Duyệt thất bại!";
+                 return RedirectToAction("TourReview");
+
+            }
+        }
+
+    }
+}
